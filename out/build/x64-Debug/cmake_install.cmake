ae29544d583504cae8e19cc2084a739b8ed0694c
N1180883 <N1180883@my.ntu.ac.uk>
2026-04-06 11:55:48 +0100

    Commit with Fixes

--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/N1180883/OneDrive/Desktop/Degree/Computer Science/Soft Eng/GPS2026
+# Install script for directory: C:/Soft Eng/GPS2026
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/N1180883/OneDrive/Desktop/Degree/Computer Science/Soft Eng/GPS2026/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Soft Eng/GPS2026/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/N1180883/OneDrive/Desktop/Degree/Computer Science/Soft Eng/GPS2026/out/build/x64-Debug/install_local_manifest.txt"
+  file(WRITE "C:/Soft Eng/GPS2026/out/build/x64-Debug/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/N1180883/OneDrive/Desktop/Degree/Computer Science/Soft Eng/GPS2026/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Soft Eng/GPS2026/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
